252bcab79262868d6a28e213b6a15292d9b93158
ASPP Student <student@localhost>
2023-08-30 12:53:41 +0300

    Brew expert potion

--- brewing/brew_potions.py
+++ brewing/brew_potions.py
@@ -1,7 +1,8 @@
-import brewing.potion_class
-import brewing.containers
-import brewing.cooking
-import brewing.inspection
+from brewing import potion_class
+from brewing import containers
+from brewing import cooking
+from brewing import inspection
+from brewing import ingredients
 
 
 def make_example_potion(student_name="ASPP student"):
@@ -13,15 +14,19 @@ def make_example_potion(student_name="ASPP student"):
     print(f"You successfully ran make_example_potion, {student_name}, well done :).")
     return my_potion
 
-
 def make_python_expert_potion(student_name):
+    my_potion = potion_class.Potion(student_name=student_name)
     print("I am a Python Expert")
     # todo: write this function!
-    return
+    my_potion.setup(container=containers.pewter_cauldron, heat_source=cooking.fire)
+    my_potion.add_ingredients(ingredients=[ingredients.fish_eyes, ingredients.unicorn_hair, ingredients.tea_leaves])
+    cooking.simmer(my_potion, duration=2)
+    return my_potion
 
 
 if __name__ == "__main__":
-    my_name = 'ASPP student'
-    my_potion = make_example_potion(student_name=my_name)
+    my_name = 'Claire and Prisca'
+    my_potion = make_python_expert_potion(student_name=my_name)
     # Let Snape inspect the potion
-    inspection.inspection_by_Snape(potion=my_potion, target_potion='example_potion')
+    inspection.inspection_by_Snape(potion=my_potion, target_potion='python_expert')
+    
